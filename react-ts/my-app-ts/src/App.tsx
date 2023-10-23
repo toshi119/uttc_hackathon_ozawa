a605d802702b8d1ee98bd3971707c76d59016ccf
Ozawa Kazumasa <ozawa23499@gmail.com>
2023-10-23 10:40:03 +0900

    Main

--- react-ts/my-app-ts/src/App.tsx
+++ react-ts/my-app-ts/src/App.tsx
@@ -3,11 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import SignIn from './Auth/SignIn';
 import SignUp from './Auth/SignUp';
-
+import Main from './Main/Main'
+import AddItemForm from './Main/AddItemForm';
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<AddItemForm />} />
+        <Route path="/" element={<Main />} />
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
