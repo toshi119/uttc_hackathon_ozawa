15c8cb0d98d446ae21c77f32f485783e996ba940
Ozawa Kazumasa <ozawa23499@gmail.com>
2023-11-11 01:53:20 +0900

    file

--- react-ts/my-app-ts/src/Main/SearchItem/FileContentRenderer.tsx
+++ react-ts/my-app-ts/src/Main/SearchItem/FileContentRenderer.tsx
@@ -1,35 +1,69 @@
+
 const renderFileContent = (item: any) => {
     switch (item.fileType) {
       case 'jpeg':
       case 'png':
       case 'gif':
       case 'svg+xml':
-        return <img src={item.file} alt="アップロードされた画像" style={{ maxWidth: '100%',width: 'auto', height: '70%', margin: '0 auto' }} />;
+        return (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <img src={item.file} alt="アップロードされた画像" style={{ maxWidth: '100%', height: 'auto', width: '75%', margin: '0 auto' }} />
+          </div>
+        );
       case 'pdf':
-        return <iframe title="PDF Viewer" src={item.file} style={{ width: '80%', height: '70%', margin: '0 auto' }} />;
+        return (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <iframe title="PDF Viewer" src={item.file} style={{ width: '80%', height: 'auto', margin: '0 auto' }} />
+          </div>
+        );
       case 'txt':
+        return (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <pre style={{ maxWidth: '100%', margin: '0 auto' }}>{item.content}</pre>
+          </div>
+        );
       case 'msword':
-        return <pre style={{ margin: '0 auto', textAlign: 'center' }}>{item.content}</pre>;
+        return (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <iframe
+              title="Word Viewer"
+              src={`https://docs.google.com/gview?url=${encodeURIComponent(item.file)}&embedded=true`}
+              style={{ width: 'auto', height: '75%', margin: '0 auto' }}
+            />
+          </div>
+        );
       case 'mp3':
-        return <audio controls src={item.file} style={{ margin: '0 auto', display: 'block' }} />;
+        return (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <audio controls src={item.file} style={{ margin: '0 auto', display: 'block', width: '70%' }} />
+          </div>
+        );
       case 'mp4':
-        return <video controls width="70%" height="auto" src={item.file} style={{ margin: '0 auto', display: 'block' }} />;
+        return (
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <video controls width="60%" height="auto" src={item.file} style={{ margin: '0 auto', display: 'block' }} />
+          </div>
+        );
       case 'vnd.ms-powerpoint':
       case 'vnd.ms-excel':
         return (
-          <iframe
-            title={item.fileType === 'vnd.ms-powerpoint' ? 'PowerPoint Viewer' : 'Excel Viewer'}
-            src={`https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(item.file)}`}
-            style={{ width: '70%', height: '500px', margin: '0 auto' }}
-          />
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <iframe
+              title={item.fileType === 'vnd.ms-powerpoint' ? 'PowerPoint Viewer' : 'Excel Viewer'}
+              src={`https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(item.file)}`}
+              style={{ width: '70%', height: '500px', margin: '0 auto' }}
+            />
+          </div>
         );
       case '':
         return null;
       default:
         return (
-          <a href={item.file} target="_blank" rel="noopener noreferrer" style={{ display: 'block', textAlign: 'center' }}>
-            ファイルをダウンロード
-          </a>
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <a href={item.file} target="_blank" rel="noopener noreferrer" style={{ display: 'block', textAlign: 'center' }}>
+              ファイルをダウンロード
+            </a>
+          </div>
         );
     }
   };
